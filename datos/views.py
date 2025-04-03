8b3b2ddd57062750fc650edc42ec646664747d80
Fernando Gonzalez Rojas <fernandodgr@hotmail.com>
2025-04-03 12:29:34 -0300

    Tercera version: más datos de personajes y plantilla html con más formato

--- datos/views.py
+++ datos/views.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 
 def index(request):
     personas = [
-        {"nombre" : "Homero" , "apellido" : "Simpson" , "descripcion" : "Padre"},
+        {"nombre": "Homero", "apellido": "Simpson", "descripcion": "Padre"},
+        {"nombre": "Marge", "apellido": "Simpson", "descripcion": "Madre"},
+        {"nombre": "Bart", "apellido": "Simpson", "descripcion": "Hijo"},
+        {"nombre": "Lisa", "apellido": "Simpson", "descripcion": "Hija"},
+        {"nombre": "Maggie", "apellido": "Simpson", "descripcion": "Hija"},
+        {"nombre": "Ned", "apellido": "Flanders", "descripcion": "Vecino"},
+        {"nombre": "Montgomery", "apellido": "Burns", "descripcion": "Jefe"},
+        {"nombre": "Seymour", "apellido": "Skinner", "descripcion": "Director"}
     ]
     contexto = {"personas" : personas}
     return render(request , "datos/index.html" , context = contexto)
